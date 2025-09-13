94a130d3ee480a7cb8b2e48e5f28d036d4034d41
Seu Nome <seuemail@exemplo.com>
2025-09-13 09:43:56 -0300

    adicionando fronte end

--- core/urls.py
+++
@@ -1,9 +0,0 @@
-# core/urls.py
-
-from django.urls import path
-from . import views # Importa as views do app (o arquivo views.py que acabamos de editar)
-
-urlpatterns = [
-    # Quando alguém acessar a URL "converter/", chame a classe CurrencyConverterView
-    path('converter/', views.CurrencyConverterView.as_view(), name='converter'),
-]
